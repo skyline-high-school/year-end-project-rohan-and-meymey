a179ea772c264f53824393ecf72c2e73960bb7b3
rohanpuri30 <rohanpuri@outlook.com>
2023-06-01 14:25:53 -0700

    new changes

--- G/src/Word.java
+++ G/src/Word.java
@@ -1,13 +1,11 @@
 import java.util.*;
 
-public class Word {
-    char[] word;
-
-    public Word(String[] words){
+class RandomWordGenerator {
+    public String getRandomWord() {
+        String[] WORDS = {"Apple", "Beach", "Brain", "Bread", "Brush", "Chair", "Chest", "Chord", "Click", "Clock", "Cloud", "Dance", "Diary", "Drink", "Earth", "Flute", "Fruit", "Ghost", "Grape", "Green", "Happy", "Heart", "House", "Juice", "Light", "Money", "Party", "Pizza", "River", "Radio", "Sheep", "Smile", "Snack", "Snake", "Spice", "Storm", "Table", "Toast", "Tiger", "Train", "Water", "Tiger", "Woman", "World", "Write", "Youth", "Abyss", "Ample", "Ankle", "Brisk", "Clasp", "Drape", "Elite", "Flair", "Gloom", "Haste", "Ivory", "Joust", "Knead", "Mourn", "Nanny", "Olive", "Poise", "Quake", "Ravel", "Swish", "Tonic", "Tweak"};
         Random rand = new Random();
-        String temp = words[rand.nextInt(words.length)];
-
-        word = temp.toCharArray();
-
+        int randomInt = (int)(Math.random()*WORDS.length);
+        String temp = WORDS[randomInt];
+        return temp;
     }
 }
--- G/src/main.java
+++ G/src/main.java
@@ -2,37 +2,118 @@ import java.io.*;
 import java.util.*;
 
 public class main {
+   
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Welcome to Guess! A Word guessing game. The player has 5 guesses to guess either a single character or the whole of a mystery word.");
+        System.out.println();
+        System.out.println("If you manage to guess a character, you will recieve an additional guess!");
+        System.out.println("Please input your name below and press enter");
+        Scanner name = new Scanner(System.in);
+        String user = name.nextLine();
+        System.out.println();
+        
+        RandomWordGenerator wordGenerator = new RandomWordGenerator();
+        String secretWord = wordGenerator.getRandomWord();
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Welcome to Guess! A two Word guessing game. The player has 10 chances to guess two words, each chance you can guess one character or a word and it will fill in the blanks, or announce if the guess is incorrect. if the word is not found by the 10th try, the user is forced to guess the two words.");
+        // player starts guessing//
+        int attempts = 5;
+        char[] current = new char[secretWord.length()];
+        for (int i = 0; i < current.length; i++) {
+            current[i] = '_';
+        }
 
-        Word word1 = new Word(Words);
-        Word word2 = new Word(Words);
+        while (attempts > 0) {
+            System.out.println(user + ", you have " + attempts + " attempts left.");
+            System.out.println("Current word: " + String.valueOf(current));
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Enter Word or Character");
-            String input = sc.next();
-            if (input.length() == 1) {
-                setChar(word1, input.toCharArray()[0], 1);
+            System.out.print("Enter a letter or the full word guess: ");
+            String guess = scan.nextLine();
+
+            boolean isNumeric = true;
+
+            try {
+                Double check = Double.parseDouble(guess);
+            } catch (NumberFormatException ex) {
+                isNumeric = false;
             }
 
+            if(isNumeric) {
+                System.out.println();
+                System.out.println("Please enter either a single character or a word.");
+                System.out.println();
+            }
+
+            // when the user guesses the word directly//
+            if (guess.equalsIgnoreCase(secretWord)) {
+                System.out.println("Congratulations! You guessed the word correctly.");
+                break;
+            } else if (guess.length() == 1) {
+                char letter = guess.charAt(0);
+                //boolean correct variable will create a conditional if the gussed letter is found anywhere in the String//
+                boolean correct = false;
+                //checking the mystery word for the guessed character//
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if ((secretWord.toLowerCase().charAt(i) == letter )) {
+                        correct = true;
+                        current[i] = letter;
+                        //user will get one more attempt for each correct guess//
+                        attempts++;
+                    }
+                }
+                if (correct) {
+                    System.out.println();
+                    System.out.println("Good guess!");
+                    System.out.println();
+                } else {
+                    System.out.println();
+                    System.out.println("Incorrect guess.");
+                    System.out.println();
+                    //attempts decrement when an inccorect guess is made//
+                    attempts--;
+                }
+            } else {
+                System.out.println();
+                System.out.println("Incorrect guess");
+                System.out.println();
+                attempts--;
+            }
+            //if the word is fully guessed either through characters or direct answer//
+            if (String.valueOf(current).equalsIgnoreCase(secretWord)) {
+                System.out.println("Congratulations! You guessed the word correctly.");
+                System.out.println();
+                System.out.println("The word was " + secretWord);
+                System.out.println();
+                System.out.println("Thanks for playing!");
+                //break used to end the loop as the user won the game//
+                break;
+            }
+        }
+
+        //conditional so that when attempts reaches zero the user gets a message the game is over and they lost//
+        if (attempts == 0) {
+                System.out.println("You lost because you ran out of attempts. The word was: " + secretWord);
+                System.out.println();
+                System.out.println("Thanks for playing!");
+                System.out.println();
         }
     }
 
-    public static String[] Words = {"Apple", "Beach", "Brain", "Bread", "Brush", "Chair", "Chest", "Chord", "Click", "Clock", "Cloud", "Dance", "Diary", "Drink", "Earth", "Flute", "Fruit", "Ghost", "Grape", "Green", "Happy", "Heart", "House", "Juice", "Light", "Money", "Party", "Pizza", "River", "Radio", "Sheep", "Smile", "Snack", "Snake", "Spice", "Storm", "Table", "Toast", "Tiger", "Train", "Water", "Tiger", "Woman", "World", "Write", "Youth", "Abyss", "Ample", "Ankle", "Brisk", "Clasp", "Drape", "Elite", "Flair", "Gloom", "Haste", "Ivory", "Joust", "Knead", "Mourn", "Nanny", "Olive", "Poise", "Quake", "Ravel", "Swish", "Tonic", "Tweak"};
-    public static char[] User1 = new char[5];
-    public static char[] User2 = new char[5];
-
-    public static void setChar(Word word, char key, int wordNum) {
-        for (int i = 0; i < 5; i++) {
-            if(key == word.charAt(i)){
-                if(wordNum == 1)
-                    User1[i] = key;
-                if(wordNum == 2)
-                    User2[i] = key;
+    public static boolean inArray(char a, char[] current) {
+        for (char b : current) {
+            if(a == b) {
+                return true;
             }
         }
+        return false;
     }
-
 }
+
+
+
+
+
+
+
+
